af84299a917c7516d39f3f205ed189742f54c35f
dima887 <movchan.1997@inbox.ru>
2022-04-20 20:30:17 +0300

    Глава 2

---
+++ Chapter02/getForm.php
@@ -0,0 +1,15 @@
+<html lang="ru">
+<head>
+    <title>Документ с формой</title>
+        <meta charset='utf-8'>
+</head>
+    <body>
+        <form action="action.php" method="GET">
+        Введите имя:
+        <input type="text" name="name" value="dima"><br>
+        Введите дату рождения:
+        <input type="text" name="born" value="29-03-1997"><br>
+        <input type="submit" value="Нажмите кнопку">
+        </form>
+    </body>
+</html>
