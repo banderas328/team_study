b96549d2c83b64decc27f95db51ccdd2ff425cfa
dima887 <movchan.1997@inbox.ru>
2022-05-17 19:59:28 +0300

    Работа с СУБД MySQL

---
+++ dima887/Chapter37/addnews.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once("connect_db.php");
+
+/**
+ * @var $pdo;
+ */
+
+/**
+ * Добавление новостного сообщения в базу данных
+ */
+
+try {
+    // Проверяем, заполнены ли поля HTML-формы
+    if (empty($_POST['name'])) exit('Не заполнено поле "Название"');
+    if (empty($_POST['content'])) exit('Не заполнено поле "Содержимое"');
+
+    // Добавляем новостное сообщение в таблицу news
+    $query = "INSERT INTO news VALUES (NULL, :name, NOW())";
+    $news = $pdo->prepare($query);
+    $news->execute(['name' => $_POST['name']]);
+
+    // Получаем только что сгенерированный идентификатор news_id
+    $news_id = $pdo->lastInsertId();
+
+    // Вставляем содержимое новостного сообщения в таблицу news_contents.
+    // Формируем запросы
+    $query = "INSERT INTO news_contents
+              VALUES (NULL, :content, :news_id)";
+    $news = $pdo->prepare($query);
+    $news->execute(['content' => $_POST['content'], 'news_id' => $news_id]);
+
+    // Осуществляем переадресацию на главную страницу
+    header("Location: news.php");
+} catch (PDOException $e) {
+    echo "Ошибка выполнения запроса: " . $e->getMessage();
+}
