902e16e511ae9c27252164ce62911268ac84fdd4
dima887 <movchan.1997@inbox.ru>
2022-05-20 19:04:26 +0300

    Работа с сетью

---
+++ dima887/Chapter39/handler.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * POST-обработчик формы.
+ */
+
+if(!empty($_POST)) {
+    echo "Имя - ".htmlspecialchars($_POST['name'])." <br />";
+    echo "Пароль - ".htmlspecialchars($_POST['pass'])." <br />";
+}
